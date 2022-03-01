771f3e0413b93ddc1e67a86f1acad549f3297921
dym <cjsxsggd@163.com>
2022-03-01 15:34:42 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
+  /* render: function(createElement) {
+    return createElement(App)
+  }, */
+  /* render(createElement) {
+    return createElement(App)
+  } */
 }).$mount('#app')
