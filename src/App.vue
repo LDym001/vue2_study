048dc8ceacdc760ecd6af504e3e0185486ba69d4
dym <cjsxsggd@163.com>
2022-03-05 15:55:14 +0800

    add a simple  Vue component demo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <todo-tip/>
   </div>
 </template>
 
 <script>
+import TodoTip from "./components/TodoTip.vue"
 
 export default {
   name: 'App',
   components: {
-    
+    TodoTip
   }
 }
 </script>
